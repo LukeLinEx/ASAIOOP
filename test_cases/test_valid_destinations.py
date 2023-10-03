1e6136564917c6f424edbba58b8bd4e5dc314c44
Tsung-Yin Lin <tsung-yinlin@Tsung-Yins-MacBook-Pro-2.local>
2023-10-02 21:56:23 -0400

    add tests chatgpt given

--- test_cases/test_valid_destinations.py
+++ test_cases/test_valid_destinations.py
@@ -115,3 +115,57 @@ s = """[[2  1  3]
 location = 4
 current_player = -1
 viz_test_valid_destination_case(s, location, current_player)
+
+
+def test_generate_valid_destinations():
+    print("\nChatGPT Tests Result:")
+    env = AnimalShogiEnv()
+
+    # Test Lion movement for Player 1 (Piece 1)
+    env.board = np.zeros(12, dtype=np.int8)
+    env.board[4] = 1  # Lion at the center
+    assert set(env.generate_valid_destinations(1, 4)) == {0,1,2,3,5,6,7,8}
+
+    # Test Giraffe movement for Player 1 (Piece 2)
+    env.board = np.zeros(12, dtype=np.int8)
+    env.board[4] = 2  # Giraffe at the center
+    assert set(env.generate_valid_destinations(2, 4)) == {1, 3, 5, 7}
+
+    # Test Elephant movement for Player 1 (Piece 3)
+    env.board = np.zeros(12, dtype=np.int8)
+    env.board[4] = 3  # Elephant at the center
+    assert set(env.generate_valid_destinations(3, 4)) == {0, 2, 6, 8}
+
+    # Test Chick movement for Player 1 (Piece 4)
+    env.board = np.zeros(12, dtype=np.int8)
+    env.board[4] = 4  # Chick at the center
+    assert set(env.generate_valid_destinations(4, 4)) == {7}
+
+    # Test Chicken movement for Player 1 (Piece 5)
+    env.board = np.zeros(12, dtype=np.int8)
+    env.board[4] = 5  # Chicken at the center
+    assert set(env.generate_valid_destinations(5, 4)) == {1, 3, 5, 7}
+
+    # Test Lion movement for Player 2 (Piece -1)
+    env.board = np.zeros(12, dtype=np.int8)
+    env.board[4] = -1  # Lion at the center
+    env.current_player = -1  # Switching to Player 2
+    assert set(env.generate_valid_destinations(-1, 4)) == {1, 3, 5, 0, 2, 6, 7, 8}
+
+    # Test Chick movement for Player 2 (Piece -1)
+    env.board = np.zeros(12, dtype=np.int8)
+    env.board[4] = -4  # Lion at the center
+    env.current_player = -1  # Switching to Player 2
+    assert set(env.generate_valid_destinations(-4, 4)) == {1}
+
+    print("All tests passed!")
+
+
+test_generate_valid_destinations()
+s = """[[0  0  0]
+[ 0  3  0]
+[ 0  0  0]
+[-3 -1 -2]]"""
+location = 4
+current_player = 1
+viz_test_valid_destination_case(s, location, current_player)
